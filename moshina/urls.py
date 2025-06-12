3e04dde6b53eb9bcd3c3a8546d5e4c94b6a845d4
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-12 17:00:46 +0500

    Info  page  tayyor

--- moshina/urls.py
+++ moshina/urls.py
@@ -2,6 +2,6 @@ from  django.urls import path
 from  .views import main, moshina_lst, moshina_info
 urlpatterns = [
     path('', main, name='main'),
-    path('moshina_lst/', moshina_lst, name='moshina_lst'),
-    path('moshina_info/<int:id>', moshina_info, name='moshina_info'),
+    path('moshina/', moshina_lst, name='moshina_lst'),
+    path('moshina/<int:id>', moshina_info, name='moshina_info'),
 ]
