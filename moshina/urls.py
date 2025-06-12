8cffa26262c5b204e4bc406c819414ab919c767e
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-12 16:41:55 +0500

    Malumotlarni  ko'rish sahifasi  uchun  path  yaratildi

--- moshina/urls.py
+++ moshina/urls.py
@@ -1,7 +1,7 @@
 from  django.urls import path
-from  .views import main, moshina_lst
-
+from  .views import main, moshina_lst, moshina_info
 urlpatterns = [
     path('', main, name='main'),
     path('moshina_lst/', moshina_lst, name='moshina_lst'),
+    path('moshina_info/<int:id>', moshina_info, name='moshina_info'),
 ]
