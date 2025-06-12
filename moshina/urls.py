d974d1699d113dedb7cf8fa07c1d62eda9aa68ee
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-12 16:05:48 +0500

    Main  page  tayyor

--- moshina/urls.py
+++ moshina/urls.py
@@ -1,6 +1,7 @@
 from  django.urls import path
-from  .views import main
+from  .views import main, moshina_lst
 
 urlpatterns = [
     path('', main, name='main'),
+    path('moshina/', moshina_lst, name='moshina_lst'),
 ]
--- moshina/views.py
+++ moshina/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Moshina
 
 # Create your views here.
 def main(request):
     return render(request, 'main.html')
+
+
+def  moshina_lst(request):
+    moshina = Moshina.objects.all()
+    context = {'moshina': moshina}
+    return render(request, 'moshina/moshina_lst.html', context = context)
