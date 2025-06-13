fdc03a7f72abbfff562ce20415cdcd1eb751765c
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-13 17:58:14 +0500

    'yuklandi'

--- avtasalon_prayekt/settings.py
+++ avtasalon_prayekt/settings.py
@@ -73,24 +73,19 @@ WSGI_APPLICATION = 'avtasalon_prayekt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'USER': 'postgres',
-#         'NAME': 'avtasalon',
-#         'PASSWORD': '1234',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3' ,
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'postgres',
+        'NAME': 'avtasalon',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
